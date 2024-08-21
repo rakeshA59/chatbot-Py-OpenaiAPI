5f0ba4e5e1d626e9778271eb19207f73be41aaa2
rakeshA59 <143877236+rakeshA59@users.noreply.github.com>
2024-08-21 17:21:13 -0500

    Update main.py secured secret key

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
 import openai
-openai.api_key= "sk-BThFw02drVCSXgEBvtC6873hglm1_8xOg3DgHN152MT3BlbkFJXcY4zvRpCT11dCAZNLgB7unBI8xdTG36BtmfcXUYsA "
+openai.api_key= "**** "
 def chat_with_GPT(prompt):
     response= openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
